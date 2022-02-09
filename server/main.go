af21747be154b45c622b4daac9ff761d869ad017
Arya Utama Putra <arya@ordent.co>
2022-02-09 12:58:10 +0700

    fix cors

--- server/main.go
+++ server/main.go
@@ -256,7 +256,7 @@ func cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if allowedOrigin(r.Header.Get("Origin")) {
 			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, OPTION, POST, PUT, PATCH, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
 		}
 		if r.Method == "OPTIONS" {
