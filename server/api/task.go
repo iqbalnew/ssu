721231be1d2f2b565fa52455ac95afeba834a370
galangrakawgs <63637942+galangrakawgs@users.noreply.github.com>
2022-02-15 11:30:59 +0700

    add CreateUser

--- server/api/task.go
+++ server/api/task.go
@@ -10,6 +10,7 @@ import (
 	announcement_pb "bitbucket.bri.co.id/scm/addons/addons-task-service/server/lib/stub/announcement_service"
 	company_pb "bitbucket.bri.co.id/scm/addons/addons-task-service/server/lib/stub/company_service"
 	notification_pb "bitbucket.bri.co.id/scm/addons/addons-task-service/server/lib/stub/notification_service"
+	users_pb "bitbucket.bri.co.id/scm/addons/addons-task-service/server/lib/stub/user_service"
 
 	pb "bitbucket.bri.co.id/scm/addons/addons-task-service/server/lib/server"
 	"github.com/sirupsen/logrus"
@@ -477,7 +478,28 @@ func (s *Server) SetTask(ctx context.Context, req *pb.SetTaskRequest) (*pb.SetTa
 				return nil, err
 			}
 			logrus.Println(res)
+		case "User":
+			var opts []grpc.DialOption
+			opts = append(opts, grpc.WithInsecure())
 
+			usersConn, err := grpc.Dial(getEnv("USER_SERVICE", ":9095"), opts...)
+			if err != nil {
+				logrus.Errorln("Failed connect to User Service: %v", err)
+				return nil, status.Errorf(codes.Internal, "Internal Error")
+			}
+			defer usersConn.Close()
+
+			client := users_pb.NewApiServiceClient(usersConn)
+
+			data := users_pb.CreateUserRequest{}
+			json.Unmarshal([]byte(task.Data), &data.Data)
+
+			data.TaskID = task.TaskID
+			res, err := client.CreateUser(ctx, &data)
+			if err != nil {
+				return nil, err
+			}
+			logrus.Println(res)
 		}
 	}
 
