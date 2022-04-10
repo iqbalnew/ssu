d53b46117b5ca02a004829ae34d3f22a69cb2795
Arya Utama Putra <arya@ordent.co>
2022-04-10 13:12:41 +0700

    update stub

--- server/lib/stub/menu_service/menu.payload.validator.pb.go
+++ server/lib/stub/menu_service/menu.payload.validator.pb.go
@@ -7,11 +7,11 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "github.com/golang/protobuf/proto"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	_ "github.com/golang/protobuf/ptypes/struct"
 	_ "github.com/mwitkow/go-proto-validators"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2/options"
+	_ "google.golang.org/protobuf/types/known/timestamppb"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
 
@@ -194,10 +194,10 @@ func (this *ListMenuLicenseResponse) Validate() error {
 	return nil
 }
 func (this *MenuAppearance) Validate() error {
-	for _, item := range this.DisableList {
+	for _, item := range this.Disables {
 		if item != nil {
 			if err := github_com_mwitkow_go_proto_validators.CallValidatorIfExists(item); err != nil {
-				return github_com_mwitkow_go_proto_validators.FieldError("DisableList", err)
+				return github_com_mwitkow_go_proto_validators.FieldError("Disables", err)
 			}
 		}
 	}
