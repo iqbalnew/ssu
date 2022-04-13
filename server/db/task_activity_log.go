4887b7d63fed12eb3de142f52006147b8d2e6052
Arya Utama Putra <arya@ordent.co>
2022-04-14 03:32:20 +0700

    update log save

--- server/db/task_activity_log.go
+++ server/db/task_activity_log.go
@@ -10,10 +10,14 @@ import (
 type ActivityLog struct {
 	TaskID  uint64
 	Command string
+	Type    string
 	Data    *pb.Task
 }
 
 func (p *GormProvider) SaveLog(ctx context.Context, log *ActivityLog) error {
+	if log.Type == "" && log.Data != nil {
+		log.Type = log.Data.Type
+	}
 	save, err := p.mongo.Collection.InsertOne(ctx, log)
 	if err != nil {
 		return err
