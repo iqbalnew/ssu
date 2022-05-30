6f25aa8021c46f97574d86d3b61fb35345ce4c9f
Arya Utama Putra <arya@ordent.co>
2022-05-30 10:21:53 +0700

    return to bongo

--- server/mongodb/mongodb.go
+++ server/mongodb/mongodb.go
@@ -1,7 +1,6 @@
 package mongoclient
 
 import (
-	"github.com/globalsign/mgo"
 	"github.com/go-bongo/bongo"
 	"github.com/sirupsen/logrus"
 )
@@ -9,7 +8,7 @@ import (
 type MongoDB struct {
 	connection *bongo.Connection
 	Collection *bongo.Collection
-	session    *mgo.Session
+	// session    *mgo.Session
 }
 
 func NewCLient(connection string, database string, collection string) *MongoDB {
@@ -17,41 +16,42 @@ func NewCLient(connection string, database string, collection string) *MongoDB {
 	logrus.Println("Mongo DB - connection string: ", connection)
 	logrus.Println("Mongo DB - db name: ", database)
 
-	// config := &bongo.Config{
-	// 	ConnectionString: connection,
-	// 	Database:         database,
-	// }
+	config := &bongo.Config{
+		ConnectionString: connection,
+		Database:         database,
+	}
 
-	// bongoConnection, err := bongo.Connect(config)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// coll := bongoConnection.Collection(collection)
-
-	session, err := mgo.Dial(connection)
+	bongoConnection, err := bongo.Connect(config)
 	if err != nil {
 		logrus.Errorln("Mongo DB - Lib: mgo")
 		panic(err)
 	}
 
+	coll := bongoConnection.Collection(collection)
+
+	// session, err := mgo.Dial(connection)
+	// if err != nil {
+	// 	logrus.Errorln("Mongo DB - Lib: mgo")
+	// 	panic(err)
+	// }
+
 	// col1 := session.DB(database).C(collection)
 
 	logrus.Printf("Mongo Db - Connected")
 
-	// return &MongoDB{
-	// 	connection: bongoConnection,
-	// 	Collection: coll,
-	// }
 	return &MongoDB{
-		session: session,
+		connection: bongoConnection,
+		Collection: coll,
 	}
+	// return &MongoDB{
+	// 	session: session,
+	// }
 }
 
 func (m *MongoDB) Close() {
 	logrus.Print("Closing Mongo Client Connection ... ")
-	// m.connection.Session.Close()
-	m.session.Close()
+	m.connection.Session.Close()
+	// m.session.Close()
 	logrus.Println("Closing Mongo Client Success")
 }
 
