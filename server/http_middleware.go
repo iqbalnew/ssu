6e9c3db156e83877b60f0536e0c6f70142b0eece
Arya Utama Putra <arya@ordent.co>
2022-06-27 13:18:09 +0700

    update log handler

--- server/http_middleware.go
+++ server/http_middleware.go
@@ -81,6 +81,8 @@ func logRequestHandler(h http.Handler, logger *addonsLogger.Logger) http.Handler
 		ri.refCode = r.Header.Get("App-Reference-Code")
 		ri.entryCode = r.Header.Get("App-Entry-Code")
 
+		fmt.Println(ri)
+
 		logger.InfoWithData("Httplog", &ri)
 	}
 	return http.HandlerFunc(fn)
