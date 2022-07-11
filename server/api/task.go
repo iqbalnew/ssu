ce853d2299dacc5ba50f5bbcf7c52235aabc384f
Fariz Rachman Yusuf <farizrachmanyusuf@gmail.com>
2022-07-11 09:49:06 +0700

    temporary disable BG Mapping from approval

--- server/api/task.go
+++ server/api/task.go
@@ -781,7 +781,7 @@ func checkAllowedApproval(md metadata.MD, taskType string, permission string) bo
 	allowed := false
 	authorities := []string{}
 	//TODO: REVISIT LATTER, skip beneficary and cash polling
-	skipProduct := []string{"SSO:User", "SSO:Company", "SSO:Client", "Menu:Appearance", "Menu:License", "Cash Pooling", "Liquidity", "Beneficiary Account"}
+	skipProduct := []string{"SSO:User", "SSO:Company", "SSO:Client", "Menu:Appearance", "Menu:License", "Cash Pooling", "Liquidity", "Beneficiary Account", "BG Mapping"}
 
 	for _, v := range skipProduct {
 		if v == taskType {
