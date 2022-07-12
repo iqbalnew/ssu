f71883fdd27800855e71021820d5ee2025c9d420
iqbalnew <abduljal44@gmail.com>
2022-07-12 16:27:48 +0700

    fix description savelog

--- server/api/task.go
+++ server/api/task.go
@@ -1879,7 +1879,9 @@ func (s *Server) SetTask(ctx context.Context, req *pb.SetTaskRequest) (*pb.SetTa
 			return nil, err
 		}
 	}
-
+	if updatedTask.Reasons != "" && req.Action == "approve" {
+		updatedTask.Reasons = ""
+	}
 	logrus.Println("Save LOG task 'update', type: ", task.Type)
 	// Save activity Log
 	if getEnv("ENV", "LOCAL") != "LOCAL" {
