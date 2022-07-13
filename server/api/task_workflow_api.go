88a32914375ef4b3c22f5b8d16fac5b5cf28eb0a
Arya Utama Putra <arya@ordent.co>
2022-07-13 13:55:06 +0700

    handle pending save task

--- server/api/task_workflow_api.go
+++ server/api/task_workflow_api.go
@@ -50,7 +50,7 @@ func (s *Server) SaveTaskWithWorkflow(ctx context.Context, req *pb.SaveTaskReque
 			isSave = true
 			task.Status = pb.Statuses_Approved
 		}
-	case "pending":
+	default:
 		if task.Status != pb.Statuses_Pending {
 			// process to pending
 			isSave = true
