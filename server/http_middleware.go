9f398ee113c0c7e4630b48b72354b6249b7fa9e7
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-07-13 15:48:49 +0700

    revert update

--- server/http_middleware.go
+++ server/http_middleware.go
@@ -21,16 +21,17 @@ type generatedCode struct {
 func setHeaderHandler(h http.Handler, sid *shortid.Shortid) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Permitted-Cross-Domain-Policies", "master-only")
+		w.Header().Set("Permissions-Policy", "default-src 'self'")	
+		w.Header().Set("Referrer-Policy", "no-referrer")
+		
 
 		if allowedOrigin(r.Header.Get("Origin")) {
 			// w.Header().Set("Content-Security-Policy", "default-src 'self'")
-			w.Header().Set("X-Content-Type-Options", "nosniff")
-			w.Header().Set("Content-Security-Policy", "default-src 'self'")
-			w.Header().Set("X-Frame-Options", "DENY")
-			w.Header().Set("X-XSS-Protection", "1; mode=block")
-			w.Header().Set("X-Permitted-Cross-Domain-Policies", "master-only")
-			w.Header().Set("Permissions-Policy", "default-src 'self'")
-			w.Header().Set("Referrer-Policy", "no-referrer")
 			w.Header().Set("Access-Control-Allow-Origin", strings.Join(config.CorsAllowedOrigins, ", "))
 			w.Header().Set("Access-Control-Allow-Methods", strings.Join(config.CorsAllowedMethods, ", "))
 			w.Header().Set("Access-Control-Allow-Headers", strings.Join(config.CorsAllowedHeaders, ", "))
