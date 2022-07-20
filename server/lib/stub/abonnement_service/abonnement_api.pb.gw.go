db347c0a27b72997a2840838c8c0d0381f6582d0
shafhik <shafhi.kasyfillah@gmail.com>
2022-07-20 13:20:09 +0700

    update abd stubs

--- server/lib/stub/abonnement_service/abonnement_api.pb.gw.go
+++ server/lib/stub/abonnement_service/abonnement_api.pb.gw.go
@@ -845,6 +845,24 @@ func local_request_ApiService_GetTasksApprovedBy_0(ctx context.Context, marshale
 
 }
 
+func request_ApiService_CompanyStatus_0(ctx context.Context, marshaler runtime.Marshaler, client ApiServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.CompanyStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_ApiService_CompanyStatus_0(ctx context.Context, marshaler runtime.Marshaler, server ApiServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq Empty
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.CompanyStatus(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterApiServiceHandlerServer registers the http handlers for service ApiService to "mux".
 // UnaryRPC     :call ApiServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -1266,7 +1284,7 @@ func RegisterApiServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/abonnement.service.v1.ApiService/GenerateBulkInvoice", runtime.WithHTTPPathPattern("/api/invoice/generate"))
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/abonnement.service.v1.ApiService/GenerateBulkInvoice", runtime.WithHTTPPathPattern("/api/abonnement/invoice-generate-bulk"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -1328,6 +1346,30 @@ func RegisterApiServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		}
 
 		forward_ApiService_GetTasksApprovedBy_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_ApiService_CompanyStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/abonnement.service.v1.ApiService/CompanyStatus", runtime.WithHTTPPathPattern("/api/abonnement/company-status"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_ApiService_CompanyStatus_0(ctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ApiService_CompanyStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1734,7 +1776,7 @@ func RegisterApiServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/abonnement.service.v1.ApiService/GenerateBulkInvoice", runtime.WithHTTPPathPattern("/api/invoice/generate"))
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/abonnement.service.v1.ApiService/GenerateBulkInvoice", runtime.WithHTTPPathPattern("/api/abonnement/invoice-generate-bulk"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -1792,6 +1834,27 @@ func RegisterApiServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
+	mux.Handle("GET", pattern_ApiService_CompanyStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/abonnement.service.v1.ApiService/CompanyStatus", runtime.WithHTTPPathPattern("/api/abonnement/company-status"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_ApiService_CompanyStatus_0(ctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ApiService_CompanyStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1830,11 +1893,13 @@ var (
 
 	pattern_ApiService_ChargeCompany_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "abonnement", "charge", "invoiceID"}, ""))
 
-	pattern_ApiService_GenerateBulkInvoice_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "invoice", "generate"}, ""))
+	pattern_ApiService_GenerateBulkInvoice_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "abonnement", "invoice-generate-bulk"}, ""))
 
 	pattern_ApiService_GetTasksCreatedBy_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "abonnement", "task", "me", "created-by"}, ""))
 
 	pattern_ApiService_GetTasksApprovedBy_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "abonnement", "task", "me", "approved-by"}, ""))
+
+	pattern_ApiService_CompanyStatus_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "abonnement", "company-status"}, ""))
 )
 
 var (
@@ -1877,4 +1942,6 @@ var (
 	forward_ApiService_GetTasksCreatedBy_0 = runtime.ForwardResponseMessage
 
 	forward_ApiService_GetTasksApprovedBy_0 = runtime.ForwardResponseMessage
+
+	forward_ApiService_CompanyStatus_0 = runtime.ForwardResponseMessage
 )
