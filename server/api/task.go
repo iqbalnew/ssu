ccf977fc375b3c5942cd240950c6bda7f4bcb89a
alfianyuandika <alfianyuandikaputra@gmail.com>
2022-07-26 13:23:04 +0700

    test delete company

--- server/api/task.go
+++ server/api/task.go
@@ -1223,44 +1223,46 @@ func (s *Server) SetTask(ctx context.Context, req *pb.SetTaskRequest) (*pb.SetTa
 
 	case "delete":
 		taskPb, _ := task.ToPB(ctx)
-		if currentStatus == 3 {
-			return &pb.SetTaskResponse{
-				Error:   false,
-				Code:    200,
-				Message: "Task Status Already Returned",
-				Data:    &taskPb,
-			}, nil
-		}
-
-		taskType := []string{"BG Mapping", "BG Mapping Digital"}
-
-		if currentStatus == 4 {
-			if !contains(taskType, task.Type) {
+		if currentStatus == 6 {
+			if currentStatus == 3 {
 				return &pb.SetTaskResponse{
 					Error:   false,
 					Code:    200,
-					Message: "Task Status Already Approved",
+					Message: "Task Status Already Returned",
 					Data:    &taskPb,
 				}, nil
 			}
-		}
 
-		if currentStatus == 5 {
-			return &pb.SetTaskResponse{
-				Error:   false,
-				Code:    200,
-				Message: "Task Status Already Rejected",
-				Data:    &taskPb,
-			}, nil
-		}
+			taskType := []string{"BG Mapping", "BG Mapping Digital"}
 
-		if currentStatus == 7 {
-			return &pb.SetTaskResponse{
-				Error:   false,
-				Code:    200,
-				Message: "Task Already Deleted",
-				Data:    &taskPb,
-			}, nil
+			if currentStatus == 4 {
+				if !contains(taskType, task.Type) {
+					return &pb.SetTaskResponse{
+						Error:   false,
+						Code:    200,
+						Message: "Task Status Already Approved",
+						Data:    &taskPb,
+					}, nil
+				}
+			}
+
+			if currentStatus == 5 {
+				return &pb.SetTaskResponse{
+					Error:   false,
+					Code:    200,
+					Message: "Task Status Already Rejected",
+					Data:    &taskPb,
+				}, nil
+			}
+
+			if currentStatus == 7 {
+				return &pb.SetTaskResponse{
+					Error:   false,
+					Code:    200,
+					Message: "Task Already Deleted",
+					Data:    &taskPb,
+				}, nil
+			}
 		}
 
 		task.LastApprovedByID = 0
