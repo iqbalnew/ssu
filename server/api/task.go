1bf48708208eafd7bb642941f99536a151cd2691
Fariz Rachman Yusuf <farizrachmanyusuf@gmail.com>
2022-08-02 16:00:15 +0700

    skip approval check for bg issuing

--- server/api/task.go
+++ server/api/task.go
@@ -802,7 +802,9 @@ func checkAllowedApproval(md metadata.MD, taskType string, permission string) bo
 	allowed := false
 	authorities := []string{}
 	//TODO: REVISIT LATTER, skip beneficary and cash polling
-	skipProduct := []string{"SSO:User", "SSO:Company", "SSO:Client", "Menu:Appearance", "Menu:License", "Cash Pooling", "Liquidity", "Beneficiary Account", "BG Mapping", "BG Mapping Digital", "Deposito"}
+	skipProduct := []string{"SSO:User", "SSO:Company", "SSO:Client", "Menu:Appearance", "Menu:License", "Cash Pooling", "Liquidity", "Beneficiary Account", "BG Mapping", "BG Mapping Digital", "BG Issuing", "Deposito"}
+
+	logrus.Print(taskType)
 
 	for _, v := range skipProduct {
 		if v == taskType {
