64dd2155f3e47a0567bb16f494bb33e9bd468321
Fariz Rachman Yusuf <farizrachmanyusuf@gmail.com>
2022-09-17 00:42:05 +0700

    update

--- server/api/task.go
+++ server/api/task.go
@@ -1646,11 +1646,21 @@ func (s *Server) SetTask(ctx context.Context, req *pb.SetTaskRequest) (*pb.SetTa
 					}
 					task.Status = 7
 
-					// if contains([]string{"BG Mapping", "BG Mapping Digital"}, task.Type) {
-					// 	task.Status = 4
-					// 	task.Step = 3
-					// 	task.Data = task.DataBak
-					// }
+					if contains([]string{"BG Mapping"}, task.Type) {
+
+						mappingDigital, err := s.provider.GetListTask(ctx, &pb.TaskORM{
+							Type:      "BG Mapping Digital",
+							CompanyID: task.CompanyID,
+						}, &pb.PaginationResponse{}, &db.QueryBuilder{}, []uint64{})
+						if err != nil {
+							return nil, status.Errorf(codes.Internal, "Internal Error: %v", err)
+						}
+
+						if len(mappingDigital) > 0 {
+							return nil, status.Errorf(codes.InvalidArgument, "Bad Request: Delete BG Mapping Failed: BG Mapping used to Mapping Digital BG [User]")
+						}
+
+					}
 				}
 				// }
 				// if currentStatus == 6 {
