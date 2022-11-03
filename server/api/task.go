568612b4b89e446e66bcd03ee37aa405f766fcad
Fariz Rachman Yusuf <farizrachmanyusuf@gmail.com>
2022-11-03 08:53:09 +0700

    :wrench: fix bg mapping and bg mapping digital approval

--- server/api/task.go
+++ server/api/task.go
@@ -1210,7 +1210,7 @@ func checkAllowedApproval(md metadata.MD, taskType string, permission string) bo
 	allowed := false
 	authorities := []string{}
 	//TODO: REVISIT LATTER, skip beneficary and cash polling
-	skipProduct := []string{"Internal Fund Transfer", "External Fund Transfer", "Payroll Transfer"}
+	skipProduct := []string{"BG Mapping", "BG Mapping Digital", "Internal Fund Transfer", "External Fund Transfer", "Payroll Transfer"}
 
 	logrus.Print(taskType)
 
