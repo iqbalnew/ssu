10ffeb40f149572b16a11f351e07aea9f5ed90c6
Fariz Rachman Yusuf <farizrachmanyusuf@gmail.com>
2022-12-16 09:57:42 +0700

    🩹 fix pending task

--- server/db/task.go
+++ server/db/task.go
@@ -119,10 +119,9 @@ func (p *GormProvider) GetGraphPendingTaskWithWorkflow(ctx context.Context, serv
 					workflow_doc->'workflow'->>'participantUserIDs' IS NULL
 					OR '%d' != ANY (TRANSLATE(workflow_doc->'workflow'->>'participantUserIDs', '[]', '{}')::INT[])
 				)
-				AND created_by_id != '%d'
 			)
 			OR ("type" = 'Payroll Transfer' AND created_by_id = '%d' AND data->>'status' = 'Ready to Submit')
-		)`, whereOpt, roleIDs, accountIDs, userID, userID, userID)
+		)`, whereOpt, roleIDs, accountIDs, userID, userID)
 	}
 
 	if whereOpt != "" {
@@ -576,9 +575,9 @@ func (p *GormProvider) GetListTaskNormal(ctx context.Context, filter *pb.TaskORM
 		}
 	}
 
-	if customQuery != "" && workflowUserIDFilter > 0 {
-		customQuery = fmt.Sprintf("(%s AND created_by_id != '%d')", customQuery, workflowUserIDFilter)
-	}
+	// if customQuery != "" && workflowUserIDFilter > 0 {
+	// 	customQuery = fmt.Sprintf("(%s AND created_by_id != '%d')", customQuery, workflowUserIDFilter)
+	// }
 
 	if workflowUserIDFilter > 0 {
 		if customQuery == "" {
